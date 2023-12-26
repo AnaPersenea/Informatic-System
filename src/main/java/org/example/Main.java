7934d398bfd17e9d5642aeb9dab0052be26af586
AnaPersenea <119055170+AnaPersenea@users.noreply.github.com>
2023-12-26 18:09:51 +0200

    Add files via upload

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,76 @@
 package org.example;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-	System.out.println("code");
+        if (args.length == 0) {
+            System.out.println("No test provided!");
+            return;
+        }
+        String testName = args[0];
+        String directoryPath = "src/main/resources/" + testName;
+        String readPath = directoryPath + "/" + testName + ".in";
+        String writePath = directoryPath + "/" + testName + ".out";
+        File in = new File(readPath);
+
+        Secretariat secretar = new Secretariat();
+
+        try (Scanner scanner = new Scanner(in)) {
+            while (scanner.hasNextLine()) {
+                String fullCommand = scanner.nextLine();
+                String command = fullCommand.split(" - ")[0];
+                if (command.equals("adauga_student")) {
+                    try (FileWriter out = new FileWriter(writePath, true)) {
+                        secretar.addStudent(fullCommand, out);
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+
+                if (command.equals("citeste_mediile")) {
+                    secretar.readNotes(directoryPath);
+                }
+
+                if (command.equals("posteaza_mediile")) {
+                    try (FileWriter out = new FileWriter(writePath, true)) {
+                        secretar.postNotes(out);
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+
+                if (command.equals("contestatie")) {
+                    secretar.modifyGrade(fullCommand);
+                }
+
+                if (command.equals("adauga_curs")) {
+                    secretar.addCourse(fullCommand);
+                }
+                if (command.equals("adauga_preferinte")) {
+                    secretar.addPreferences(fullCommand);
+                }
+                if (command.equals("repartizeaza")) {
+                    secretar.allocateCourse();
+                }
+                if (command.equals("posteaza_curs")) {
+                    try (FileWriter out = new FileWriter(writePath, true)) {
+                        secretar.postCourseInfo(out, fullCommand);
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+                if (command.equals("posteaza_student")) {
+                    try (FileWriter out = new FileWriter(writePath, true)) {
+                        secretar.postStudentInfo(out, fullCommand);
+                    } catch (IOException e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
